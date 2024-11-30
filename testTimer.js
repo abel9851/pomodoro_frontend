ee9921a1c54aca6b65f2e5a11f13b13f0ef5d6e2
HeejunShin <abel9851@gmail.com>
2024-11-30 12:17:40 +0900

    chore: set up test timer layout

---
+++ testTimer.js
@@ -0,0 +1,9 @@
+const timeDisplay = document.getElementById("timeDisplay");
+const button = document.getElementById("startButton");
+
+let timerInterval = null;
+
+// 새로 배우는 것은 3번 반복하기
+function timerInterval() {
+    if (timerInverval) return; // Prevent multiple intervals
+};
